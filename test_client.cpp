e53599ddb767bb516f4bd9c8f6e1d96e58dcaed8
Caturra <wasdjkl233@gmail.com>
2022-07-25 02:30:17 +0800

    add thread config

--- test_client.cpp
+++ test_client.cpp
@@ -1,39 +1,87 @@
 #include <bits/stdc++.h>
 #include "trpc/Client.h"
 
-int main() {
-    ::signal(SIGPIPE, SIG_IGN);
+std::atomic<int> gTODO {};
+std::atomic<std::chrono::system_clock::time_point> gStart {std::chrono::system_clock::now()};
+std::atomic<std::chrono::system_clock::time_point> gEnd {std::chrono::system_clock::now()};
+
+void clientRoutine(int sessions, int startNumber, int numbers) {
+::signal(SIGPIPE, SIG_IGN);
     auto &env = co::open();
-    auto co = env.createCoroutine([&] {
-        trpc::Endpoint peer {"127.0.0.1", 2333};
-        auto pClient = trpc::Client::make(peer);
-        if(!pClient) {
-            std::cerr << "failed, abort" << std::endl;
-            return;
-        } else {
-            std::cout << "connected" << std::endl;
-        }
-        auto start = std::chrono::steady_clock::now();
-        auto client = std::move(pClient.value());
-        int sum = 0;
-        constexpr size_t count = 1e5;
-        for(size_t i = 0; i < count; ++i) {
-            auto resp = client.call<int>("add", sum, 1);
-            int old = sum;
-            if(old == (sum = resp.value_or(sum))) {
-                std::cerr << "failed at: " << i << std::endl;
+    int sum = startNumber;
+    int count = 5e5;
+    int done = 0;
+    auto start = std::chrono::steady_clock::now();
+    for(int i = 0; i < sessions; ++i) {
+        auto co = env.createCoroutine([&] {
+            trpc::Endpoint peer {"127.0.0.1", 2333};
+            auto pClient = trpc::Client::make(peer);
+            if(!pClient) {
+                std::cerr << "failed, abort" << std::endl;
+                return;
+            } else {
+                std::cout << "connected" << std::endl;
             }
-        }
-        auto end = std::chrono::steady_clock::now();
-        std::cout << "sum: " << sum << std::endl;
-        using Milli = std::chrono::duration<double, std::milli>;
-        std::cout << "per msg: " << Milli{end - start}.count() / count << "ms" << std::endl;
-        // output:
-        // connected
-        // sum: 100000
-        // per msg: 0.341057ms
-    });
-    co->resume();
+            auto client = std::move(pClient.value());
+            for(size_t j = 0; j < numbers; ++j) {
+                int old = sum;
+                // add first, other clients won't send the same request
+                sum++;
+                auto resp = client.call<int>("add", old, 1);
+                if(old + 1 != resp.value()) {
+                    std::cerr << "failed at: " << i << ":" << j << std::endl;
+                }
+            }
+
+            done++;
+            if(done == sessions) {
+                if(--gTODO == 0) {
+                    gEnd = std::chrono::system_clock::now();
+                    std::cout << "done, press any key" << std::endl;
+                }
+            }
+        });
+        co->resume();
+    }
+    
     co::loop();
+}
+
+int main(int argc, const char *argv[]) {
+    int threads = 1;
+    if(argc > 1) {
+        threads = ::atoi(argv[1]);
+    }
+    gTODO = threads;
+
+    // per thread
+    int sessions = 1;
+    if(argc > 2) {
+        sessions = ::atoi(argv[2]);
+    }
+
+    // total
+    int count = 1e5;
+    if(argc > 3) {
+        count = ::atoi(argv[3]);
+    }
+    gStart = std::chrono::system_clock::now();
+
+    for(int i = 0; i < threads; ++i) {
+        std::thread {clientRoutine, sessions, i * count, count / threads}.detach();
+    }
+
+    int c; std::cin>>c;
+
+    using Milli = std::chrono::duration<double, std::milli>;
+    auto perMsg = Milli{gEnd.load() - gStart.load()}.count() / count / threads;
+    auto qps = 1000 / perMsg;
+    std::cout << "per msg: " << perMsg << "ms" << std::endl;
+    std::cout << "QPS: " << qps << std::endl;
+    // 4750U
+    // server: 10
+    // client: 8 2 100000
+    // per msg: 0.00633958ms
+    // QPS: 157739
     return 0;
 }
--- test_server.cpp
+++ test_server.cpp
@@ -5,11 +5,13 @@ std::string append(std::string a, std::string b) {
     return a+b;
 }
 
-int main() {
+void createServer() {
     ::signal(SIGPIPE, SIG_IGN);
     auto pServer = trpc::Server::make({"127.0.0.1", 2333});
     std::cout << bool(pServer) << std::endl;
-    if(!pServer) return 1;
+    if(!pServer) {
+        std::cerr << "cannot create server." << std::endl;
+    }
     auto server = std::move(pServer.value());
     using namespace std::chrono_literals;
 
@@ -22,4 +24,15 @@ int main() {
     });
     listener->resume();
     co::loop();
+}
+
+int main(int argc, const char *argv[]) {
+    int threads = 1;
+    if(argc > 1) {
+        threads = ::atoi(argv[1]);
+    }
+    for(int i = 1; i < threads; ++i) {
+        std::thread {createServer}.detach();
+    }
+    createServer();
 }
